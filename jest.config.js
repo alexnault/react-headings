24807c2fd19c274f92b61999f9384a8be20f89be
Alex Nault <nault.alex@gmail.com>
2021-01-03 01:02:38 -0500

    chore(): Add 100% coverage threshold

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,12 @@ module.exports = {
   testEnvironment: "jsdom",
   collectCoverage: true,
   coverageReporters: ["json", "lcov", "text", "clover", "text-summary"],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
 };
