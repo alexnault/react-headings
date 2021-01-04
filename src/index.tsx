688874b8376aa390eb11fae887209d09041c4f94
Alex Nault <nault.alex@gmail.com>
2021-01-03 21:33:36 -0500

    refactor(): Use template literal type for Heading

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 type HeadingLevel = 1 | 2 | 3 | 4 | 5 | 6;
 
-type Heading = "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+type Heading = `h${HeadingLevel}`;
 
 const LevelContext = React.createContext<HeadingLevel>(1);
 
